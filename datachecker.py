f9053564ba44d59466f2b66d368788676c8892e7
belieffs22 <beliefs22@gmail.com>
2015-11-22 21:05:26 -0500

    most recent changes

--- datachecker.py
+++ datachecker.py
@@ -47,16 +47,27 @@ def check_date(date):
     test_pattern(date,pattern)
 
 def check_word(word):
-    
+    word = word.lower()
     patterns = ['[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}','[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{2,4}',
-               'January','February','March','April','May','June',
-               'July','August','September','October','December','^\d+\.\d+$']
+                '[0-9]{1,2}-[0-9]{1,2}-[0-9]{2,4}','January','February','March','April',
+                'May','June','July','August','September','October','December','^\d+\.\d+$']
     for pattern in patterns:
         if re.search(pattern,word):
             return True
+
+    punctuation = ['.',',',':','"',]
+
+    for mark in punctuation:
+        if mark.endswith(mark):
+            new_word = word[:len(word)-1]
+            if re.search(pattern,new_word):
+                return True
+            else:
+                return new_word in dictionary or new_word in firstnames \
+                       or new_word in lastnames or new_word.isdigit()
         
-    return word.lower() in dictionary or word.lower() in firstnames \
-           or word.lower() in lastnames or word.isdigit()
+    return word in dictionary or word in firstnames \
+           or word in lastnames or word.isdigit()
     
 
 def main():
@@ -64,11 +75,12 @@ def main():
     print check_word("1/2/2015")
     print check_word("September, 2015")
     print check_word("1.2.2015")
+    print check_word("1-2-2015")
     print check_word("1.2")
     print check_word("quentin")
     print check_word("Quentin")
     print check_word("fractures")
-    print check_word("felkajsefe")
+    print check_word("fractures.")
 
     
 
