cc4fd35e07553d4478727ea68c6d80f729eefc01
belieffs22 <beliefs22@gmail.com>
2015-12-13 20:06:10 -0500

    adding gui

--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@ from tkFileDialog import *
 from tkMessageBox import *
 from extractexcel import *
 
+global root
+root = Tk()
 
 def OpenFile():
     name = askopenfilename()
@@ -13,19 +15,57 @@ def CleanData(allowed, not_allowed, indeterminate):
 def Run():
     excelfile = OpenFile()
     Excel_File = Excel(excelfile)
-    allowed, not_allowed, indeterminate = Excel_File.create_word_list()
-    
-    Excel_File.create_final_csv()
+    master_allowed, master_not_allowed, master_indeterminate = Excel_File.create_word_lists()
+    user_allowed, user_not_allowed = Find(master_allowed, master_not_allowed, master_indeterminate)
+    Excel_File.create_user_dicts(user_allowed, user_not_allowed)
+    Excel_File.clean_data
     excelfile.close()
+def display_list(list_to_display,label_text):
+    pass
     
+def Find(master_allowed, master_not_allowed, master_indeterminate):
+    global root
+
+            
+    yScroll = Scrollbar(orient=VERTICAL)
+    yScroll.grid(row=0, column=1, sticky=N+S)
+    title = Label(root, text='Please select allowed words')
+    select = Button(root, text='Select Words', command=SelectWords)
+    select.grid(row=
+    title.grid(row=0, column=0, sticky=E+W)
+    indeterm_list = Listbox(root, yscrollcommand=yScroll.set, activestyle ='dotbox', selectmode=MULTIPLE)
+    indeterm_list.grid(row=1, column=0, sticky=N+S+E+W)
+    yScroll['command'] = indeterm_list.yview
+    spot = 0
+    for word in master_indeterminate:
+        indeterm_list.insert(spot, word)
+        spot = spot + 1
+        
+    def SelectWords(listbox=indeterm_list):
+        print listbox.curselection()
+
     
-def About(hello):
-    print hello
+    return master_allowed, master_not_allowed    
+    
+
+class Checkbar(Frame):
+    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
+        Frame.__init__(self, parent)
+        self.vars = []
+        for pick in picks:
+            var = IntVar()
+            chk = Checkbutton(self, text=pick, variable=var, indicatoron=0)
+            chk.pack(side=side, anchor=anchor, expand=YES)
+            self.vars.append(var)
+        def state(self):
+            return map((lambda var: var.get()), self.vars)
+
+    
+def About():
+    
     
     showinfo("About","A program to De-Identify data")
 
-
-root = Tk()
 menu = Menu(root)
 root.config(menu=menu)
 filemenu = Menu(menu)
